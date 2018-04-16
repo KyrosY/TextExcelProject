0e586232de16545200f50ea0f8a6d9c218891056
KyrosY <19yipk83@millshighsch.org>
2018-04-16 08:54:13 -0700

    Confused

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -22,41 +22,46 @@ public class FormulaCell extends RealCell {
 	public double getDoubleValue() {
 		double value = 0;
 		String[] formulaParts = getValue().split(" ");
-		for(int i=1; i< formulaParts.length-1; i++) {
-			
-			if (formulaParts[i].equals("+")) {
-				value+= Double.parseDouble(formulaParts[i+1]);
+		if(formulaParts[1].toLowerCase().equals("sum")) {
+			String[] cellRef= formulaParts[2].split("-");
+			value = sum(cellRef[0], cellRef[1]);
+		} else if (formulaParts[1].toLowerCase().equals("avg")) {
+			String[] cellReference = formulaParts[2].split("-");
+			value = average(cellReference[0], cellReference[1]);
+		} else {
+			for(int j=1; j<formulaParts.length-1; j++) {
+				if(Character.isLetter(formulaParts[j].charAt(0))) {
+					Location loc = new SpreadsheetLocation(formulaParts[j]);
+					Cell[][] sheet;
+					formulaParts[j] = ((RealCell) sheet[loc.getRow()][loc.getCol()]).getDoubleValue() +"";	
+				}
+			}
+			value = Double.parseDouble(formulaParts[1]);
+			for(int i=2; i< formulaParts.length-1; i++) {
+				value = operations(value, formulaParts[i], formulaParts[i+1]);
 				i++;
-				
-			} else if (formulaParts[i].equals("-")) {
-				value-= Double.parseDouble(formulaParts[i+1]);
-				i++;
-				
-			} else if (formulaParts[i].equals("*")) {
-				value*= Double.parseDouble(formulaParts[i+1]);
-				i++;
-				
-			} else if (formulaParts[i].equals("/")){
-				value/= Double.parseDouble(formulaParts[i+1]);
-				i++;
-			} else {
-				value += Double.parseDouble(formulaParts[i]);
-			}//	else {
+			}
 		}
 		return value;
 	}
-	/*
-	public double operations(double operator1, String operand, double operator2) {
-		if (operand.equals("+")) {
-			operator1 += operator2;
-		} else if (operand.equals("-")) {
-			operator1 -= operator2;
-		} else if (operand.equals("*")) {
-			operator1 *= operator2;
-		} else if (operand.equals("/")){
-			operator1 /= operator2;
+	public double operations(double num1, String operator, String operand) {
+		double val = 0;
+		double num2 = Double.parseDouble(operand);
+		if (operator.equals("+")) {
+			val = num1 + num2;
+		} else if (operator.equals("-")) {
+			val = num1 - num2;
+		} else if (operator.equals("*")) {
+			val = num1 * num2;
+		} else if (operator.equals("/")){
+			val = num1 / num2;
 		}
-		return operator1;
+		return val;
 		}
-		*/
+	public double sum (String cell1, String cell2) {
+		return 0;
+	}
+	public double average(String cell1, String cell2) {
+		return 0;
+	}
 }
